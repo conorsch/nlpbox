736a5fc3ed1fdf8557b5039fca62842f94da17be
Conor Schaefer <conor.schaefer@gmail.com>
2014-06-04 18:36:13 -0400

    adds "srilm" as Python module

--- provisioning/scripts/compile_swig_srilm.sh
+++ provisioning/scripts/compile_swig_srilm.sh
@@ -5,3 +5,5 @@ perl -p -i -e 's#/home/speech/Resources/Tools/(srilm/include)#/opt/$1#' $SWIGSRI
 perl -p -i -e 's#/opt/python/2.7/include/python2.7#/usr/include/python2.7#' $SWIGSRILM/Makefile
 
 make -C $SWIGSRILM python
+
+cp $SWIGSRILM/{_srilm.so,srilm.py} /usr/lib/python2.7
