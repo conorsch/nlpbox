3eb809c04edf1d749111137eac12520adea51cbd
Conor Schaefer <conor.schaefer@gmail.com>
2014-06-14 23:13:56 -0400

    ensures SRILM PATH is set

--- provisioning/scripts/personalize_env.sh
+++ provisioning/scripts/personalize_env.sh
@@ -1,15 +1,16 @@
 gitdir=/home/vagrant/gits
-mkdir $gitdir
+su vagrant -c "mkdir -p $gitdir"
 
-aptitude install homesick
-gem install homesick
+sudo gem install homesick
 
-cd $gitdir && su vagrant -c 'git clone https://github.com/nojhan/liquidprompt.git'
-su vagrant -c 'homesick clone https://github.com/ronocdh/dotfiles.git'
-su vagrant -c 'homesick symlink --force dotfiles'
-
+su vagrant
+cd $gitdir 
+git clone https://github.com/nojhan/liquidprompt.git
+homesick clone https://github.com/ronocdh/dotfiles.git
+homesick symlink --force dotfiles
 srilm_path_declaration='PATH="$SRILM/bin/i686-ubuntu:$PATH"'
-new_srilm_path=$(echo "$srilm_path_declaration" | perl -npe 's#(i686)-ubuntu#$1#')
-if [[ ! grep "$new_srilm_path" /home/vagrant/.bashrc ]]; then
-    echo $($new_srilm_path >> /home/vagrant/.bashrc;
-fi
+new_srilm_path="$(echo "$srilm_path_declaration" | perl -npe 's#(i686)-ubuntu#$1#')"
+grep -vq "$new_srilm_path" /home/vagrant/.bashrc && echo "$new_srilm_path" >> /home/vagrant/.bashrc
+exit
+
+source /home/vagrant/.bashrc
