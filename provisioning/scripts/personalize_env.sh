88535720f2be77496a672552f044de7cc96c7872
Conor Schaefer <conor.schaefer@gmail.com>
2014-06-15 00:23:11 -0400

    run env setup as vagrant user, not root

--- provisioning/scripts/personalize_env.sh
+++ provisioning/scripts/personalize_env.sh
@@ -3,14 +3,12 @@ su vagrant -c "mkdir -p $gitdir"
 
 sudo gem install homesick
 
-su vagrant
 cd $gitdir 
-git clone https://github.com/nojhan/liquidprompt.git
-homesick clone https://github.com/ronocdh/dotfiles.git
-homesick symlink --force dotfiles
-srilm_path_declaration='PATH="$SRILM/bin/i686-ubuntu:$PATH"'
+su vagrant -c 'git clone https://github.com/nojhan/liquidprompt.git'
+su vagrant -c 'homesick clone https://github.com/ronocdh/dotfiles.git'
+su vagrant -c 'homesick symlink --force dotfiles'
+
+srilm_path_declaration='export PATH="$SRILM/bin/i686-ubuntu:$PATH"'
 new_srilm_path="$(echo "$srilm_path_declaration" | perl -npe 's#(i686)-ubuntu#$1#')"
 grep -vq "$new_srilm_path" /home/vagrant/.bashrc && echo "$new_srilm_path" >> /home/vagrant/.bashrc
-exit
-
 source /home/vagrant/.bashrc
