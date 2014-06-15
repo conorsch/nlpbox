1dbea39ece8d8b315e6e91a037a248348d878d2f
Conor Schaefer <conor.schaefer@gmail.com>
2014-06-14 21:47:51 -0400

    comments out useless autodoc feature

--- provisioning/scripts/compile_swig_srilm.sh
+++ provisioning/scripts/compile_swig_srilm.sh
@@ -5,6 +5,7 @@ git clone https://github.com/desilinguist/swig-srilm $SWIGSRILM
 perl -p -i -e 's#/home/speech/Resources/Tools/(srilm/lib/i686)-m64#/opt/$1#' $SWIGSRILM/Makefile
 perl -p -i -e 's#/home/speech/Resources/Tools/(srilm/include)#/opt/$1#' $SWIGSRILM/Makefile
 perl -p -i -e 's#/opt/python/2.7/include/python2.7#/usr/include/python2.7#' $SWIGSRILM/Makefile
+#echo '%feature("autodoc");' >> $SWIGSRILM/srilm_python.i
 
 make -C $SWIGSRILM python
 
