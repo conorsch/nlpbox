b5ce14422d7bb945d899b9c3d5b6cd73faff4524
Conor Schaefer <conor.schaefer@gmail.com>
2014-06-14 21:54:53 -0400

    adds substitution for SRILM PATH

--- provisioning/scripts/personalize_env.sh
+++ provisioning/scripts/personalize_env.sh
@@ -7,3 +7,9 @@ gem install homesick
 cd $gitdir && su vagrant -c 'git clone https://github.com/nojhan/liquidprompt.git'
 su vagrant -c 'homesick clone https://github.com/ronocdh/dotfiles.git'
 su vagrant -c 'homesick symlink --force dotfiles'
+
+srilm_path_declaration='PATH="$SRILM/bin/i686-ubuntu:$PATH"'
+new_srilm_path=$(echo "$srilm_path_declaration" | perl -npe 's#(i686)-ubuntu#$1#')
+if [[ ! grep "$new_srilm_path" /home/vagrant/.bashrc ]]; then
+    echo $($new_srilm_path >> /home/vagrant/.bashrc;
+fi
