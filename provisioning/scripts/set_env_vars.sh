e5aa83a1777600c97f2f274771c93373dadb0d66
Conor Schaefer <conor.schaefer@gmail.com>
2014-06-25 08:10:33 -0400

    adds custom NLTK_DATA path to match custom env

--- provisioning/scripts/set_env_vars.sh
+++ provisioning/scripts/set_env_vars.sh
@@ -1,5 +1,6 @@
 cat <<ENVIRONMENT_VARIABLES >> /etc/environment
 SWIGSRILM='/opt/swig-srilm'
 SRILM='/opt/srilm'
+NLTK_DATA='/home/vagrant/.nltk_data'
 ENVIRONMENT_VARIABLES
 source /etc/environment
