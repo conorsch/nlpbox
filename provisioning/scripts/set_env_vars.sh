02cc05f41612f695fb6bf2242bab2432b09e60d6
Conor Schaefer <conor.schaefer@gmail.com>
2014-06-25 11:18:23 -0400

    forces SRILM PATH setup for all users

--- provisioning/scripts/set_env_vars.sh
+++ provisioning/scripts/set_env_vars.sh
@@ -4,3 +4,10 @@ SRILM='/opt/srilm'
 NLTK_DATA='/home/vagrant/.nltk_data'
 ENVIRONMENT_VARIABLES
 source /etc/environment
+
+cat <<SRILMPATHSETUP > /etc/profile.d/srilmpath.sh
+source /etc/environment
+PATH="$SRILM/bin:$PATH"
+PATH="$SRILM/bin/$(uname -m):$PATH"
+export PATH
+SRILMPATHSETUP
